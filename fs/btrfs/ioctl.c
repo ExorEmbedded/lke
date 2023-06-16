df56290906825c026715d9139dd49f1dfe460a78
Luca Bassanello <luca.bassanello@dataedge.it>
2023-06-16 18:30:30 +0200

    BSP-4839 CVE-2023-1611 Mark affected code

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -1,3 +1,4 @@
+#error CVE-2023-1611
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2007 Oracle.  All rights reserved.
--- fs/btrfs/qgroup.c
+++ fs/btrfs/qgroup.c
@@ -1,3 +1,4 @@
+#error CVE-2023-1611
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2011 STRATO.  All rights reserved.
