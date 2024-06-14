826ec7c29f92277d19e6d5640f08de6435641e6d
Luca Bassanello <luca.bassanello@dataedge.it>
2024-06-14 16:51:41 +0200

    BSP-5664 CVE-2023-52448 mark affected code

--- fs/gfs2/rgrp.c
+++ fs/gfs2/rgrp.c
@@ -1,3 +1,4 @@
+#error CVE-2023-52448
 /*
  * Copyright (C) Sistina Software, Inc.  1997-2003 All rights reserved.
  * Copyright (C) 2004-2008 Red Hat, Inc.  All rights reserved.
@@ -2618,4 +2619,3 @@ void gfs2_rlist_free(struct gfs2_rgrp_list *rlist)
 		rlist->rl_ghs = NULL;
 	}
 }
-
